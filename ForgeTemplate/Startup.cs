c12bd9a04c0b3da3d06483639d4fb75162116dae
AmrFayez <amr.fayez1994@gmail.com>
2021-12-03 22:31:06 +0200

    add vue client app

--- ForgeTemplate/Startup.cs
+++ ForgeTemplate/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace ForgeTemplate
 {
@@ -19,11 +15,27 @@ namespace ForgeTemplate
         }
 
         public IConfiguration Configuration { get; }
-
+        readonly string AllPolicy = "All";
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: AllPolicy,
+                                  builder =>
+                                  {
+                                      builder.AllowAnyOrigin()
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod();
+                                  });
+            });
             services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddSpaStaticFiles(options => options.RootPath = "client-app/dist");
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Forge Template", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,16 +52,35 @@ namespace ForgeTemplate
                 app.UseHsts();
             }
 
+        
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseRouting();
-
+            app.UseCors(AllPolicy);
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}").RequireCors(AllPolicy);
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{Area?}/{controller=Home}/{action=Index}/{id?}").RequireCors(AllPolicy);
+            });
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "client-app";
+                if (env.IsDevelopment())
+                {
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:8081");
+                }
             });
         }
     }
